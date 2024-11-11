7093451314b53d5780bd2d36f6f97f0240e82f24
andriguetticamila <110103333+andriguetticamila@users.noreply.github.com>
2024-11-11 09:37:47 -0300

    confirmar

---
+++ scrypt.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa__principal");
+const caixaPerguntas = document.querySelector(".caixa__perguntas");
+const caixaAlternativas = document.querySelector(".caixa__alternativas");
+const caixaResultados = document.querySelector(".caixa__resultados");
+const textoResultado = document.querySelector(".texto__resultado");
